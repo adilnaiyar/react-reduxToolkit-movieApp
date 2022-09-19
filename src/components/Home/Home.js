72b4c3a926fdf2e5084fb590a78d989e644f35fe
adilnaiyar <adil.naiyar@gmail.com>
2022-09-19 16:37:52 +0530

    redux with asynchronous action

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,24 +1,17 @@
 import React, { useEffect } from "react";
 import MovieListing from "../MovieListing/MovieListing";
-import MovieApi from "../../common/apis/MovieApi";
-import { APIKey } from "../../common/apis/MovieApiKey";
 import { useDispatch } from "react-redux";
-import { addMovies } from "../../features/movies/movieSlice";
+import {
+  fetchAsyncMovies,
+  fetchAsyncShows,
+} from "../../features/movies/movieSlice";
 
 const Home = () => {
-  const MovieName = "Harry";
   const dispatch = useDispatch();
   useEffect(() => {
-    const fetchMovies = async () => {
-      const response = await MovieApi.get(
-        `?apiKey=${APIKey}&s=${MovieName}&type=movie`
-      ).catch((error) => {
-        console.log("Err :", error);
-      });
-      dispatch(addMovies(response.data));
-    };
-    fetchMovies();
-  }, []);
+    dispatch(fetchAsyncMovies());
+    dispatch(fetchAsyncShows());
+  }, [dispatch]);
   return (
     <div>
       <div className="banner-img"></div>
--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,24 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Spinner from "../Spinnner/Spinner";
 import "./MovieCard.scss";
 
 const MovieCard = (props) => {
   const { data } = props;
   return (
     <div className="card-item">
-      <Link to={`/movie/${data.imdbID}`}>
-        <div className="card-inner">
-          <div className="card-top">
-            <img src={data.Poster} alt={data.Title} />
-          </div>
-          <div className="card-bottom">
-            <div className="card-info">
-              <h4>{data.Title}</h4>
-              <p>{data.Year}</p>
+      {Object.keys(data).length === 0 ? (
+        <Spinner />
+      ) : (
+        <Link to={`/movie/${data.imdbID}`}>
+          <div className="card-inner">
+            <div className="card-top">
+              <img src={data.Poster} alt={data.Title} />
+            </div>
+            <div className="card-bottom">
+              <div className="card-info">
+                <h4>{data.Title}</h4>
+                <p>{data.Year}</p>
+              </div>
             </div>
           </div>
-        </div>
-      </Link>
+        </Link>
+      )}
     </div>
   );
 };
